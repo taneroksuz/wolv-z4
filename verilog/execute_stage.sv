5c5c5bceb868273615a743909b4ca57e4595545f
Taner Öksüz <taner.oeksuez@outlook.de>
2023-03-28 18:10:03 +0200

    reset polarity changed. submodule updated.

--- verilog/execute_stage.sv
+++ verilog/execute_stage.sv
@@ -318,7 +318,7 @@ module execute_stage
   end
 
   always_ff @(posedge clock) begin
-    if (reset == 1) begin
+    if (reset == 0) begin
       r <= init_execute_reg;
     end else begin
       r <= rin;
