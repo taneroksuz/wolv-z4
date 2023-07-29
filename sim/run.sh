1b65f14c80e879056ecdd964582a97e08a93a996
Taner Öksüz <taner.oeksuez@outlook.de>
2023-07-29 19:34:20 +0200

    some errors fixed.

--- sim/run.sh
+++ sim/run.sh
@@ -80,7 +80,7 @@ then
   cp $BASEDIR/build/$PROGRAM/elf/$PROGRAM.host host.dat
   if [ "$DUMP" = 'on' ]
   then
-    $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME --testplusarg FILENAME=$PROGRAM.vcd
+    $XSIM soc_snapshot --tclbatch $BASEDIR/sim/xsim_cfg.tcl --wdb $PROGRAM.wdb --testplusarg MAXTIME=$MAXTIME
   else
     $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME
   fi
@@ -94,7 +94,7 @@ then
     cp $BASEDIR/build/$PROGRAM/elf/$filename.host host.dat
     if [ "$DUMP" = 'on' ]
     then
-      $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME --testplusarg FILENAME=$filename.vcd
+      $XSIM soc_snapshot --tclbatch $BASEDIR/sim/xsim_cfg.tcl --wdb $filename.wdb --testplusarg MAXTIME=$MAXTIME
     else
       $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME
     fi
@@ -107,7 +107,7 @@ else
   cp $BASEDIR/$subpath/elf/$filename.host host.dat
   if [ "$DUMP" = 'on' ]
   then
-    $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME --testplusarg FILENAME=$filename.vcd
+    $XSIM soc_snapshot --tclbatch $BASEDIR/sim/xsim_cfg.tcl --wdb $filename.wdb --testplusarg MAXTIME=$MAXTIME
   else
     $XSIM soc_snapshot -R --testplusarg MAXTIME=$MAXTIME
   fi
